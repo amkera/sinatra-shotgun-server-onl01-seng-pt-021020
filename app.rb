2377a3052e08f8fb54bc8dde21a549f50abc8a84
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-04-29 02:55:13 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,5 +6,8 @@ class App < Sinatra::Base
     "Welcome to your app!!!! I built this! "
   end
   #this is the controller action
+  
+  #localhost:9292 is the local port 
+  #localhost:9393 is the default shotgun port 
 
 end
